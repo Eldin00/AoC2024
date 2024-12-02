bfc4b47f5fea3c0a11333e1e5c7081fdeedd9ea2
Eric McKeeth <eldin00@gmail.com>
2024-12-02 00:37:41 -0700

    simplify day 2

--- src/puzzles/day_2.rs
+++ src/puzzles/day_2.rs
@@ -32,7 +32,7 @@ fn part_2(lines: Vec<String>) {
     for l in lines {
         let vs: Vec<&str> = l.as_str().split_whitespace().collect();
         let vi: Vec<u32> = vs.iter().map(|x| x.parse::<u32>().unwrap()).collect();
-        if safe_test2(vi, false) {
+        if safe_test2(vi) {
             safe_count += 1;
         }
     }
@@ -52,20 +52,17 @@ fn safe_test(list: Vec<u32>) -> bool {
     true
 }
 
-fn safe_test2(mut list: Vec<u32>, secondary: bool) -> bool {
+fn safe_test2(mut list: Vec<u32>) -> bool {
     let ascending = list[0] < list[1];
     for i in 1..list.len() {
         if (list[i - 1] < list[i]) != ascending
             || list[i].abs_diff(list[i - 1]) > 3
             || list[i] == list[i - 1]
         {
-            if secondary {
-                return false;
-            }
             let mut list_copy = list.clone();
             _ = list_copy.remove(i);
             _ = list.remove(i - 1);
-            return safe_test2(list_copy, true) || safe_test2(list, true);
+            return safe_test(list_copy) || safe_test(list);
         }
     }
     true
